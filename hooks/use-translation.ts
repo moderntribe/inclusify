869a7f4a17b1b35fca013c96c114472870c2d131
sejas <antonio@sejas.es>
2021-06-15 10:59:44 +0100

    feat(api): change to post method

--- hooks/use-translation.ts
+++ hooks/use-translation.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { useQuery, UseQueryResult } from 'react-query';
 
-const BASE_URL = 'https://europe-west1-degenderify.cloudfunctions.net/degenderify'; // 'http://0.0.0.0:8080'
+const BASE_URL = 'http://0.0.0.0:8080'; //'https://europe-west1-degenderify.cloudfunctions.net/degenderify'; // 'http://0.0.0.0:8080'
 
 type QueryKey = [string, string, string[]];
 
 async function fetchTranslation({ queryKey }: { queryKey: QueryKey }) {
   const [, text, options] = queryKey;
-  const { data } = await axios.get(BASE_URL, {
+  const { data } = await axios.post(BASE_URL, {
     params: {
       text,
       options
